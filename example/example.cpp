ff118b57000a78d5a5069e505551d1575049b34c
Tyopo131 <samecob62@gmail.com>
2025-02-27 18:23:32 +0000

    Add example library usage

--- example/example.cpp
+++ example/example.cpp
@@ -1,7 +1,11 @@
 #include "terminate/clear.h"
+#include <thread>
+#include <chrono>
+using namespace std::chrono_literals;
 
 int main() {
-	std::cout << "A thing";
-	std::cout.flush();
-	terminatelib::clear_lines();
+	std::cout << "Doing tasks...";
+	std::this_thread::sleep_for(7s); 
+	// Simulate doing tasks
+	terminatelib::clear_lines(); // Erase the output
 }
