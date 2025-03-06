e815b3beda8fd023e590e1176faace1a57ee37c3
Tyopo131 <samecob62@gmail.com>
2025-03-06 12:01:23 +0000

    Deprecate old move_cursor that takes two int arguments

--- include/terminate/cursor.h
+++ include/terminate/cursor.h
@@ -3,6 +3,7 @@
 #include <terminate/terminalpos.h>
 
 namespace terminatelib {
+	[[deprecated(R"#("move_cursor(std::optional<int> line, std::optional<int> col)" is deprecated. Please use "move_cursor(TerminalPos pos)" instead.)#")]]
 	void move_cursor(std::optional<int> line, std::optional<int> col);
 	void move_cursor(TerminalPos pos);
 }
