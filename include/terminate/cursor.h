c61ffbbf83340b2452b14f64694aecbda29e4993
Tyopo131 <samecob62@gmail.com>
2025-03-06 11:59:58 +0000

    Add new overload of move_cursor that takes a TerminalPos argument instead of two int arguments

--- include/terminate/cursor.h
+++ include/terminate/cursor.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <optional>
+#include <terminate/terminalpos.h>
 
 namespace terminatelib {
 	void move_cursor(std::optional<int> line, std::optional<int> col);
+	void move_cursor(TerminalPos pos);
 }
--- src/cursor.cpp
+++ src/cursor.cpp
@@ -1,8 +1,24 @@
-#include "cursor.h"
+#include <cursor.h>
 #include <iostream>
+#include <optional>
+#include <terminalpos.h>
+
 
 void terminatelib::move_cursor(std::optional<int> line, std::optional<int> col)
 {
+	if (!(line.has_value() || col.has_value())) {
+		throw std::runtime_error("Both parameters have no value. At least one parameter should have a value.");
+		return;
+	}
+	if (line.has_value() && !col.has_value()) std::cout << "\033[" << line.value() << 'd';
+	else if (line.has_value() && col.has_value()) std::cout << "\033[" << line.value() << ';' << col.value() << 'H';
+	else if (!line.has_value() && col.has_value()) std::cout << "\033[" << col.value() << 'G';
+}
+
+void terminatelib::move_cursor(TerminalPos pos)
+{
+	std::optional<int> line = pos.line;
+	std::optional<int> col = pos.col;
 	if (!(line.has_value() || col.has_value())) {
 		throw std::runtime_error("Both parameters have no value. At least one parameter should have a value.");
 		return;
